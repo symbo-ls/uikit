ab8005acafe411a1c3c45c70423ef1fe7d2917e3
nikoloza <nika.tomadze@gmail.com>
2020-04-21 19:02:59 +0400

    cleaning

--- src/config/theme.js
+++ src/config/theme.js
@@ -6,12 +6,4 @@ var themeMap = {
   border: 'border-color'
 }
 
-var themeA = {
-  text: 'blue',
-  background: 'white',
-  border: 'black', // .opacity(0.2),
-  helpers: [],
-  inverse: {} // schemeAInverse
-}
-
 export default { themeMap }
