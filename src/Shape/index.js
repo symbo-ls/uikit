4ff7291ba528ab2fec643ac4e03c7e6e8fdfac0f
nikoloza <nika.tomadze@gmail.com>
2020-06-12 20:37:16 +0400

    removed emotion from symbols

--- src/Shape/index.js
+++ src/Shape/index.js
@@ -1,20 +1,19 @@
 'use strict'
 
 import { theme } from 'scratch'
-import { css } from 'emotion'
-
 import { style, shape, depth, round, size } from './style'
 
 var Shape = {
   style,
 
   class: {
-    shape: element => css(shape[element.shape]),
-    depth: element => css(depth[element.depth]),
-    round: element => css(round[element.round]),
-    size: element => css(size[element.size]),
-    theme: element => css(theme[element.theme])
+    shape: element => shape[element.shape],
+    depth: element => depth[element.depth],
+    round: element => round[element.round],
+    size: element => size[element.size],
+    theme: element => theme[element.theme]
   },
+
   define: {
     shape: param => param || 'rectangle',
     depth: param => param !== undefined ? param : 10,
