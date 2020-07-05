44e50f5b6f6388bbbbb97ff3b695e4f3974e5f1a
nikoloza <nika.tomadze@gmail.com>
2020-07-05 23:30:34 +0400

    updated icon settings

--- src/Icon/index.js
+++ src/Icon/index.js
@@ -7,7 +7,7 @@ import SVG from '../SVG'
 export default {
   proto: SVG,
   style,
-  define: { name: (param, element) => param },
+  define: { name: param => param },
   file: element => sprite[element.name || element.key],
   attr: { viewBox: '0 0 16 16' }
 }
