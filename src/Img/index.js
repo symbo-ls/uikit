6d7319c644e7cb169644c0e76806960a9396503c
nikoloza <nika.tomadze@gmail.com>
2020-08-05 20:24:13 +0400

    updated img alt and title

--- src/Img/index.js
+++ src/Img/index.js
@@ -2,8 +2,14 @@
 
 export default {
   tag: 'img',
-  define: { src: param => param },
+  define: {
+    src: param => param,
+    alt: param => param || '',
+    title: param => param || ''
+  },
   attr: {
-    src: element => element.src
+    src: element => element.src,
+    alt: element => element.alt,
+    title: element => element.title || element.alt
   }
 }
