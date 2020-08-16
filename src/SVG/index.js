f59b0fa077551c3a6a514819094198ef5999d7aa
nikoloza <nika.tomadze@gmail.com>
2020-08-16 19:01:50 +0400

    udpated svg attrs, updated package

--- src/SVG/index.js
+++ src/SVG/index.js
@@ -5,6 +5,10 @@ var useSVGSymbol = file => `<use xlink:href="${file}" />`
 // create icon
 export default {
   tag: 'svg',
+  attr: {
+    'xmlns': 'http://www.w3.org/2000/svg',
+    'xmlns:xlink': 'http://www.w3.org/1999/xlink'
+  },
   define: { src: (param, element) => useSVGSymbol(param) },
   html: element => element.src || useSVGSymbol(element.key)
 }
