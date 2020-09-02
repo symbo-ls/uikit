ab95733bb00b156f4dbd99950a4ac4991fade5b5
nikoloza <nika.tomadze@gmail.com>
2020-09-02 07:34:07 +0400

    make some noise

--- showcase/svinchyShapes/index.js
+++ showcase/svinchyShapes/index.js
@@ -3,5 +3,18 @@
 import { Shape } from '../../src'
 
 export default {
+  tooltips: {
+    style: { marginBottom: 42 },
+    childProto: {
+      proto: Shape,
+      style: {
+        width: 100,
+        height: 100,
+      }
+    },
+
+    one: {}
+  },
+
   text: 'make some noise'
 }
