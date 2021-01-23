9492a3e1075a42c36fa68edc51fbac1cd3feee72
nikoloza <nika.tomadze@gmail.com>
2021-01-23 05:05:48 +0400

    bump domql

--- src/Link/index.js
+++ src/Link/index.js
@@ -4,10 +4,12 @@ export default {
   tag: 'a',
   define: {
     href: param => param,
+    target: param => param,
     aria: param => param || {}
   },
   attr: {
     href: element => element.href,
+    target: element => element.target,
     'aria-label': element => element.aria.label || element.text
   }
 }
