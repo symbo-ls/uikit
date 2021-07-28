565d7c058a814dc5e5170f6ab407c1b101b7658d
nikoloza <nika.tomadze@gmail.com>
2021-07-28 17:02:55 +0400

    orig guard

--- src/config/theme.js
+++ src/config/theme.js
@@ -4,6 +4,7 @@ var themeA = { // eslint-disable-line no-unused-vars
   text: 'blue',
   background: 'white',
   border: 'black', // .opacity(0.2),
+  active: {},
   helpers: [],
   themes: {},
   inverse: {} // schemeAInverse
@@ -17,6 +18,7 @@ export const THEME = {
 }
 
 export const returnSubThemeOrDefault = (orig, theme) => {
+  if (!orig) return
   if (orig.themes && orig.themes[theme]) return orig.themes[theme]
   if (orig[theme]) return [orig, orig[theme]]
   return orig
