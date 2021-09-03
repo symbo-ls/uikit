9e4b6874800de5eb36cdfa0c9c9ff051afb05cf3
nikoloza <nika.tomadze@gmail.com>
2021-09-03 09:00:14 +0400

    added responsive breakpoints

---
+++ src/config/responsive.js
@@ -0,0 +1,13 @@
+'use strict'
+
+export const RESPONSIVE = {
+  screenL: 1920,
+  screenD: 1680,
+  screenS: 1440,
+  tabletL: 1366,
+  tabletM: 1280,
+  tabletS: 1024,
+  mobileL: 768,
+  mobileM: 560,
+  mobileS: 480
+}
