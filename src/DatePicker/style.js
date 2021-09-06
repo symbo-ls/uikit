f329e7119959335dcf90832d2f50de27f344f125
svinchy <svinchy@gmail.com>
2021-09-06 10:05:58 +0400

    delege border

--- src/DatePicker/style.js
+++ src/DatePicker/style.js
@@ -1,10 +1,10 @@
 'use strict'
 
 export default {
-  maxWidth: '33.6em',
-  maxHeight: '26em',
+  maxWidth: `${336 / 16}em`,
+  maxHeight: `${260 / 16}em`,
   display: 'flex',
-  padding: '0 1.6em',
+  padding: '0 1em',
   boxSizing: 'border-box',
   borderRadius: '10px',
 
@@ -19,10 +19,9 @@ export default {
     display: 'flex',
     flexDirection: 'column',
     overflowX: 'auto',
-    paddingRight: '1em',
+    paddingRight: `${10 / 16}em`,
 
     button: {
-      // fontSize: '1.2em',
       opacity: 0.5,
       marginBottom: `${22 / 12}em`
     }
@@ -30,8 +29,8 @@ export default {
 
   main: {
     flex: 1,
-    paddingTop: '1.4em',
-    paddingBottom: '1em',
+    paddingTop: `${14 / 16}em`,
+    paddingBottom: `${10 / 16}em`,
     overflow: 'hidden',
 
     display: 'flex',
@@ -43,12 +42,10 @@ export default {
       height: 'auto',
       overflow: 'hidden',
       width: '100%',
-      padding: '0 .8em 1.4em .8em',
+      padding: `0 ${8 / 16}em ${14 / 16}em ${8 / 16}em`,
       boxSizing: 'border-box'
     },
     '> header > svg': {
-      // fontSize: '1.2em',
-      // color: '#11B9DC',
       cursor: 'pointer'
     },
 
@@ -58,11 +55,9 @@ export default {
       flex: 1
     },
     '> header span': {
-      // fontSize: '1.4em',
       minWidth: '100%',
       textTransform: 'capitalize',
       textAlign: 'center',
-      // color: 'black',
       fontWeight: 'bold'
     },
 
@@ -76,20 +71,16 @@ export default {
       display: 'grid',
       gridTemplateColumns: 'repeat(7, 1fr)',
       gap: '6px',
-      paddingBottom: '.2em'
+      paddingBottom: `${2 / 16}em`
     },
     'section > header span': {
-      // fontSize: '10px',
       textAlign: 'center',
-      // color: 'black',
       fontWeight: 'bold'
     },
     'section > header span:nth-child(6)': {
-      // color: 'rgba(0, 0, 0, .5)'
       opacity: 0.5
     },
     'section > header span:nth-child(7)': {
-      // color: 'rgba(0, 0, 0, .5)'
       opacity: 0.5
     },
     'section > div': {
@@ -99,14 +90,11 @@ export default {
       gap: '6px'
     },
     'section > div button': {
-      // fontSize: '1.3em'
     },
     'section > div button:nth-child(7n)': {
-      // color: 'rgba(0, 0, 0, .5)'
       opacity: 0.5
     },
     'section > div button:nth-child(7n - 1)': {
-      // color: 'rgba(0, 0, 0, .5)'
       opacity: 0.5
     }
   }
