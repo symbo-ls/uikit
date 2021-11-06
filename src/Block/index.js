dda7b24d3d9d953e74e016b698a3b3bd45011345
nikoloza <nika.tomadze@gmail.com>
2021-11-06 20:38:26 +0400

    added max and min width

--- src/Block/index.js
+++ src/Block/index.js
@@ -35,6 +35,8 @@ export const Block = {
 
   class: {
     width: ({ props }) => mapSpacing(props.width, 'width'),
+    maxWidth: ({ props }) => mapSpacing(props.maxWidth, 'max-width'),
+    minWidth: ({ props }) => mapSpacing(props.minWidth, 'min-width'),
     height: ({ props }) => mapSpacing(props.height, 'height'),
     padding: ({ props }) => mapBasedOnRatio(props, 'padding'),
     margin: ({ props }) => mapBasedOnRatio(props, 'margin'),
