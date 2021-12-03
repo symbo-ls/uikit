a05773ac3dda72949a919ce193ae3f5ec4fbba9b
nikoloza <nika.tomadze@gmail.com>
2021-12-03 06:56:14 +0400

    renaming policies

---
+++ packages/route-link/src/index.js
@@ -0,0 +1,23 @@
+'use strict'
+
+import { Link } from '@symbo.ls/link'
+import { deepMerge } from '@domql/utils'
+import { router } from '@domql/router'
+
+const RouteLink = {
+  on: {
+    click: (event, element, state) => {
+      const root = element.lookup('app')
+      const { href } = element.props
+      const firstThree = href[0] + href[1] + href[2]
+      if (href && firstThree !== 'htt' && firstThree !== 'ske') {
+        router(root, href, {})
+        event.preventDefault()
+      }
+    }
+  }
+}
+
+deepMerge(Link, RouteLink)
+
+export default RouteLink
