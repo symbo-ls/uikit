eead0b728483ef5c124876183e453c3a00bcbbf5
nikoloza <nika.tomadze@gmail.com>
2021-12-04 22:50:04 +0400

    dist of react

--- packages/datepicker/packages/react/index.js
+++ packages/datepicker/packages/react/index.js
@@ -1,1 +1,8 @@
 'use strict'
+
+import { reactTransformer } from '@domql/react-transformer'
+import { DatePicker as component } from '@symbo.ls/datepicker'
+
+export const DatePicker = (props, state) => {
+  return reactTransformer(component, props, state)
+}
