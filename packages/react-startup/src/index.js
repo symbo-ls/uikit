2bc8babe6458de56d940da8099a8d7d44e4ba0f0
nikoloza <nika.tomadze@gmail.com>
2021-12-05 15:45:15 +0400

    updated react startup

--- packages/react-startup/src/index.js
+++ packages/react-startup/src/index.js
@@ -4,15 +4,17 @@ import '@domql/emotion'
 import { set } from '@symbo.ls/scratch'
 
 import CONFIG_DEFAULT from '@symbo.ls/config-default'
-// import { loadModule } from './require'
+import { loadModule } from './require'
+
+const SYMBOLSRC_FILE = loadModule('~/symbolsrc.json')
 
 // import SYMBOLSRC_FILE from '~/symbolsrc.json'
 
 // const SYMBOLSRC_FILE = process.cwd() + '/.symbolsrc.json'
 // loadModule(SYMBOLSRC_FILE)
 
-// const activeConfig = SYMBOLSRC_FILE || CONFIG_DEFAULT
-set(CONFIG_DEFAULT)
+const activeConfig = SYMBOLSRC_FILE || CONFIG_DEFAULT
+set(activeConfig)
 
 export default CONFIG_DEFAULT
 
