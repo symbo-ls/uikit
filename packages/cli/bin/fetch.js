1c104b97b110483c28b44c5c5a9e55c383c5f028
nikoloza <nika.tomadze@gmail.com>
2021-12-05 14:41:20 +0400

    updated with cli, versions

--- packages/cli/bin/fetch.js
+++ packages/cli/bin/fetch.js
@@ -5,6 +5,7 @@ import { createRequire } from 'module'
 import { Command } from 'commander'
 import fetch from 'node-fetch'
 import chalk from 'chalk'
+import fs from 'fs'
 
 const require = createRequire(import.meta.url)
 const pkg = require('../package.json')
@@ -16,30 +17,43 @@ const DEFAULT_CONFIG = 'https://raw.githubusercontent.com/symbo-ls/uikit/8e7026a
 program
   .version(pkg.version)
 
-program
-  .command('login [destination]')
-  .description('Sign in to Symbols')
-  .argument('<username>', 'user to login')
-  .argument('[password]', 'password for user, if required', 'no password given')
-  .action(async (username, password) => {
-    console.log('username:', username)
-    console.log('password:', password)
+// program
+//   .command('login [destination]')
+//   .description('Sign in to Symbols')
+//   .argument('<username>', 'user to login')
+//   .argument('[password]', 'password for user, if required', 'no password given')
+//   .action(async (username, password) => {
+//     console.log('username:', username)
+//     console.log('password:', password)
 
-    const response = await fetch(API_URL)
-    const body = await response.json()
-    console.log(body)
-  })
+//     const response = await fetch(API_URL)
+//     const body = await response.json()
+//     console.log(body)
+//   })
 
 program
   .command('fetch [destination]')
   .description('Fetch symbols')
   .action(async (source, destination) => {
     const response = await fetch(DEFAULT_CONFIG)
-    const body = await response.text()
-    const parsed = JSON.parse(body)
-    const { version, ...config } = parsed
+    const body = await response.json()
+    const { version, ...config } = body
+
     console.log('')
     console.log(chalk.bold('Symbols'), 'config received: ', chalk.green(version))
+
+    const path = process.cwd() + '/.symbolsrc.json'
+    console.log(chalk.dim('Default config fetched: '), chalk.dim.underline(path))
+    console.log('')
+
+    fs.writeFile(path, JSON.stringify(body), err => {
+      if (err) {
+        console.log('Error writing file', err)
+      } else {
+        console.log('Successfully wrote file')
+      }
+    })
+
     for (const t in config) {
       const type = config[t]
       console.log(chalk.bold(t))
