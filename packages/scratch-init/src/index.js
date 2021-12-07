5fbc49ac8bf99ce704a31d8786bd084bcf51d820
nikoloza <nika.tomadze@gmail.com>
2021-12-07 16:52:53 +0400

    scratch init

--- packages/scratch-init/src/index.js
+++ packages/scratch-init/src/index.js
@@ -5,15 +5,26 @@ import { set } from '@symbo.ls/scratch'
 
 import CONFIG_DEFAULT from '@symbo.ls/config-default'
 import { loadModule } from './require'
+import * as PATH from 'path'
+// import PGK from '~/.symbolsrc.json'
 
-const SYMBOLSRC_FILE = loadModule('~/symbolsrc.json')
+// const appDir = dirname(require.main.filename)
+// require_isomorphic('foo')
+
+// console.log(process.cwd())
+// console.log(__dirname)
+// console.log(PATH)
+// console.log(import.meta)
+
+// console.log(PATH.resolve('file://symbolsrc.json'))
 
 // import SYMBOLSRC_FILE from '~/symbolsrc.json'
 
 // const SYMBOLSRC_FILE = process.cwd() + '/.symbolsrc.json'
-// loadModule(SYMBOLSRC_FILE)
+// console.log(PGK)
 
-const activeConfig = SYMBOLSRC_FILE || CONFIG_DEFAULT
+// const activeConfig = SYMBOLSRC_FILE || CONFIG_DEFAULT
+const activeConfig = CONFIG_DEFAULT
 console.log('Config received:')
 console.log(activeConfig)
 set(activeConfig)
--- packages/scratch-init/src/require.js
+++ packages/scratch-init/src/require.js
@@ -4,6 +4,7 @@ export const loadModule = async (modulePath) => {
   try {
     return await import(modulePath)
   } catch (e) {
-    throw new ImportError(`Unable to import module ${modulePath}`)
+    console.log(`Unable to import module ${modulePath}`)
+    // throw new ImportError(`Unable to import module ${modulePath}`)
   }
 }
