1f3450451c6da9a24f05cbd3a0090e392460843e
nikoloza <nika.tomadze@gmail.com>
2022-06-27 06:56:26 +0400

    add css from props

--- src/Box/index.js
+++ src/Box/index.js
@@ -3,5 +3,8 @@
 import { Shape, Position, Block, Text, Responsive } from '..'
 
 export const Box = {
-  proto: [Shape, Position, Block, Text, Responsive]
+  proto: [Shape, Position, Block, Text, Responsive],
+  class: {
+    fromProps: ({ props }) => props && props.css
+  }
 }
