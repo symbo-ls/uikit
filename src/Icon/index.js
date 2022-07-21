2c9b17a0dc354870f480fc19a564e8ac98593d31
nikoloza <nika.tomadze@gmail.com>
2022-07-21 05:47:46 +0400

    name prop to icon

--- src/Icon/index.js
+++ src/Icon/index.js
@@ -8,7 +8,7 @@ export const Icon = {
   proto: SVG,
   style,
   define: { name: param => param },
-  name: ({ props }) => props.icon,
+  name: ({ props }) => props.name || props.icon,
   attr: { viewBox: '0 0 24 24' },
   src: ({ key, name }) => name || key || 'noIcon'
 }
