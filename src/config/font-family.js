bad463bc208bdcf51ef1e432a9a5843aa797932f
nikoloza <nika.tomadze@gmail.com>
2022-07-21 22:57:30 +0400

    added monospace

--- src/config/font-family.js
+++ src/config/font-family.js
@@ -11,6 +11,7 @@ var defaultFont = { // eslint-disable-line
 export const FONT_FAMILY = {}
 export const FONT_FAMILY_TYPES = {
   serif: 'Helvetica, Arial, sans-serif, --system-default',
-  'sans-serif': 'Times New Roman, Georgia, serif, --system-default'
+  'sans-serif': 'Times New Roman, Georgia, serif, --system-default',
+  monospace: 'Courier New, monospace, --system-default'
 }
 export const FONT_FACE = getFontFace(FONT_FAMILY)
