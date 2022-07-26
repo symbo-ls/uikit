f318111e4fc3edff5700a304612f9e5bf5bb95f8
nikoloza <nika.tomadze@gmail.com>
2022-07-26 12:31:53 +0400

    cleanup

--- src/SVG/index.js
+++ src/SVG/index.js
@@ -2,13 +2,12 @@
 
 const useSVGSymbol = file => `<use xlink:href="${file}" />`
 
-// create icon
+// create SVG symbol
 export const SVG = {
   tag: 'svg',
   attr: {
     xmlns: 'http://www.w3.org/2000/svg',
     'xmlns:xlink': 'http://www.w3.org/1999/xlink'
   },
-  // define: { src: param => param },
   html: ({ key, props }) => useSVGSymbol(props.src)
 }
