4379bf887da1ca6d5e928739d8f35d0ae6a3e000
nikoloza <nika.tomadze@gmail.com>
2022-07-31 23:06:08 +0400

    fixed name

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 'use strict'
 
+import { merge, isFunction } from '@domql/utils'
 import { classNames } from './classNames'
 import { keySetters } from './subprops'
 
 export * from '@symbo.ls/scratch'
 
-export const transformClassname = props => {
+export const transformClassName = props => {
   const CLASS_NAMES = {}
 
   for (const key in props) {
     const setter = keySetters[key.slice(0, 1)]
     const hasCSS = classNames[key]
     if (setter) setter(key, props[key], CLASS_NAMES)
-    else if (hasCSS) CLASS_NAMES[key] = hasCSS
+    else if (isFunction(hasCSS)) merge(CLASS_NAMES, hasCSS)(props)
   }
 
   return CLASS_NAMES
