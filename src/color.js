a1f4fef3639b8b5f6cb8b270fb7d87c9becd6eb9
nikoloza <nika.tomadze@gmail.com>
2022-08-01 07:57:37 +0400

    initial commit

---
+++ src/color.js
@@ -0,0 +1,37 @@
+'use strict'
+
+export const COLOR = {
+  transparent: 'rgba(0, 0, 0, 0)',
+  black: 'black',
+  blue: '#0079FD',
+  indigo: '#50E1FF',
+  green: '#59AC56',
+  red: '#FE5B47',
+  yellow: '#EDCB38',
+  orange: '#E2862F',
+  brown: '#7D6755',
+  pink: '#FD8ABF',
+  purple: '#7345AF',
+  white: 'white'
+}
+
+export const GRADIENT = {
+  'gradient-dark': `linear-gradient(0deg,
+    rgba(0,0,0,0.06) 0%,
+    rgba(0,0,0,0.07) 100%
+  )`,
+  'gradient-dark-active': `linear-gradient(0deg,
+    rgba(0,0,0,0.09) 0%,
+    rgba(0,0,0,0.1) 100%
+  )`,
+  'gradient-light': `linear-gradient(
+    0deg,
+    rgba(255,255,255,0.05) 0%,
+    rgba(255,255,255,0.06) 100%
+  )`,
+  'gradient-light-active': `linear-gradient(
+    0deg,
+    rgba(255,255,255,0.09) 0%,
+    rgba(255,255,255,0.10) 100%
+  )`
+}
