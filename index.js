b796027628d537d73fa65b1d72d7fc8e2138cae8
nikoloza <nika.tomadze@gmail.com>
2022-08-01 06:15:23 +0400

    inject css-in-props

--- index.js
+++ index.js
@@ -4,21 +4,29 @@ import {
   set,
   FONT,
   RESET,
-  getFontFace
+  getFontFaceString
 } from '@symbo.ls/scratch'
 import CONFIG_DEFAULT from '@symbo.ls/config-default'
 import SYMBOLSRC_FILE from './.symbolsrc.json'
 
-import { injectGlobal } from '@emotion/css'
+import createEmotion from '@emotion/css/create-instance'
+import { setClassname } from 'css-in-props'
+
+const { css, injectGlobal } = createEmotion({
+  key: 'smbls'
+})
+
+export * from 'css-in-props'
 
 export const init = (config) => {
   const defaultConfig = config || CONFIG_DEFAULT || SYMBOLSRC_FILE
 
-  const Scratch = set({ verbose: false, ...defaultConfig })
-  console.log(Scratch)
+  set({ verbose: false, ...defaultConfig })
 
-  const FontFace = getFontFace(FONT)
+  const FontFace = getFontFaceString(FONT)
 
   injectGlobal(FontFace)
   injectGlobal(RESET)
 }
+
+export const setClass = props => setClassname(props, css)
