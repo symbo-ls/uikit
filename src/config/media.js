57c0c0f5f091b40ec8575700fc180c293b2ca8e7
Nika <nika.tomadze@gmail.com>
2022-08-09 20:33:10 +0400

    Update media.js

--- src/config/media.js
+++ src/config/media.js
@@ -2,7 +2,7 @@
 
 export const MEDIA = {
   screenL: '(max-width: 1920px)',
-  screenD: '(max-width: 1680px)',
+  screenM: '(max-width: 1680px)',
   screenS: '(max-width: 1440px)',
   tabletL: '(max-width: 1366px)',
   tabletM: '(max-width: 1280px)',
@@ -10,6 +10,18 @@ export const MEDIA = {
   mobileL: '(max-width: 768px)',
   mobileM: '(max-width: 560px)',
   mobileS: '(max-width: 480px)',
+  mobileXS: '(max-width: 375px)',
+  
+  sL: '(max-width: 1920px)',
+  sM: '(max-width: 1680px)',
+  sS: '(max-width: 1440px)',
+  tL: '(max-width: 1366px)',
+  tM: '(max-width: 1280px)',
+  tS: '(max-width: 1024px)',
+  mL: '(max-width: 768px)',
+  mM: '(max-width: 560px)',
+  mS: '(max-width: 480px)',
+  mS: '(max-width: 375px)',
 
   light: '(prefers-color-scheme: light)',
   dark: '(prefers-color-scheme: dark)'
