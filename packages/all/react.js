ddcf00334ebc792b6ce923b3963a0aab4786725b
nikoloza <nika.tomadze@gmail.com>
2022-08-13 03:34:52 +0400

    added provider

--- packages/all/react.js
+++ packages/all/react.js
@@ -1,8 +1,10 @@
 'use strict'
 
 import Box from '@symbo.ls/react-box'
+import { SymbolsProvider } from '@symbo.ls/react-provider'
 
 export * from '@symbo.ls/init'
 export { Box }
+export { SymbolsProvider }
 
 // export * from './styles'
--- packages/provider/index.js
+++ packages/provider/index.js
@@ -1,27 +1,27 @@
-"use strict";
+'use strict'
 
-import React from "react";
-import { set } from "@symbo.ls/scratch";
+import React from 'react'
+import { set } from '@symbo.ls/scratch'
 import CONFIG_DEFAULT from '@symbo.ls/config-default'
 
 const context = React.createContext({
-  config: {},
-});
-const Provider = context.Provider;
+  config: {}
+})
+const Provider = context.Provider
 
 export const SymbolsProvider = ({ config, children }) => {
   const defaultConfig = config || CONFIG_DEFAULT
   React.useEffect(() => {
-    set(defaultConfig);
-  }, []) 
+    set(defaultConfig)
+  }, [])
 
   return React.createElement(
     Provider,
     {
       value: {
-        config: config,
-      },
+        config: config
+      }
     },
     children
-  );
-};
+  )
+}
