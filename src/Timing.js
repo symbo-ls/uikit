86b50781e19edafd548184ef4451c0e8748ee1c2
nikoloza <nika.tomadze@gmail.com>
2022-09-17 12:52:50 +0400

    fixed transition

---
+++ src/Timing.js
@@ -0,0 +1,65 @@
+'use strict'
+
+import { isString } from '@domql/utils'
+import { getTimingByKey, getTimingFunction } from '@symbo.ls/scratch'
+
+const transformTransition = transition => {
+  const arr = transition.split(' ')
+
+  if (!arr.length) return transition
+
+  return arr.map(v => {
+    if (v.length < 3 || v.includes('ms')) {
+      const mapWithSequence = getTimingByKey(v)
+      return mapWithSequence.timing || v
+    }
+    if (getTimingFunction(v)) return getTimingFunction(v)
+    return v
+  }).join(' ')
+}
+
+const transformDuration = (duration, props, propertyName) => {
+  if (!isString(duration)) return
+  return duration.split(',').map(getTimingByKey).join(',')
+}
+
+const splitTransition = transition => {
+  const arr = transition.split(',')
+  if (!arr.length) return
+  return arr.map(transformTransition).join(',')
+}
+
+export const Timing = {
+  class: {
+    transition: ({ props }) => props.transition && ({
+      transition: splitTransition(props.transition)
+    }),
+    transitionDuration: ({ props }) => props.transitionDuration && ({
+      transitionDuration: transformDuration(props.transitionDuration)
+    }),
+    transitionDelay: ({ props }) => props.transitionDelay && ({
+      transitionDelay: transformDuration(props.transitionDelay)
+    }),
+    transitionTimingFunction: ({ props }) => props.transitionTimingFunction && ({
+      transitionTimingFunction: getTimingFunction(props.transitionTimingFunction)
+    }),
+    transitionProperty: ({ props }) => props.transitionProperty && ({
+      transitionProperty: props.transitionProperty,
+      willChange: props.transitionProperty
+    }),
+
+    animationDuration: ({ props }) => props.animationDuration && ({
+      animationDuration: transformDuration(props.animationDuration)
+    }),
+    animationDelay: ({ props }) => props.animationDelay && ({
+      animationDelay: transformDuration(props.animationDelay)
+    }),
+    animationTimingFunction: ({ props }) => props.animationTimingFunction && ({
+      animationTimingFunction: getTimingFunction(props.animationTimingFunction)
+    }),
+    animationProperty: ({ props }) => props.animationProperty && ({
+      animationProperty: props.animationProperty,
+      willChange: props.animationProperty
+    })
+  }
+}
