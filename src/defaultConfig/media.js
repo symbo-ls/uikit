5f2c9719f51503f4349dfe97d14742b8c900be13
Nika <nika.tomadze@gmail.com>
2022-10-04 04:00:49 +0400

    Update media.js

--- src/defaultConfig/media.js
+++ src/defaultConfig/media.js
@@ -14,17 +14,6 @@ export const MEDIA = {
   mobileS: '(max-width: 480px)',
   mobileXS: '(max-width: 375px)',
 
-  sL: '(max-width: 1920px)',
-  sM: '(max-width: 1680px)',
-  sS: '(max-width: 1440px)',
-  tL: '(max-width: 1366px)',
-  tM: '(max-width: 1280px)',
-  tS: '(max-width: 1024px)',
-  mL: '(max-width: 768px)',
-  mM: '(max-width: 560px)',
-  mS: '(max-width: 480px)',
-  mXS: '(max-width: 375px)',
-
   light: '(prefers-color-scheme: light)',
   dark: '(prefers-color-scheme: dark)'
 }
