e0bc6291a778a3a081be69e89d66f3bcc9117910
nikoloza <nika.tomadze@gmail.com>
2022-10-10 14:12:51 +0400

    added init

--- bin/init.js
+++ bin/init.js
@@ -5,25 +5,26 @@ import chalk from 'chalk'
 import { program } from './program.js'
 
 program
-  .command('init [destination]')
+  .command('init [name]')
   .description('Initialize a repository')
-  .option('-m, --mode', 'Which Symbols to install (domql, react)')
-  .action(async (mode) => {
-    const packageName = `@symbo.ls/${mode || 'uikit'}`
-    console.log('Adding', chalk.green.bold(packageName))
+  .argument('<name>')
+  .option('--framework', 'Which Symbols to install (domql, react)')
+  .action(async (framework) => {
+    const packageName = `@symbo.ls/${framework || 'uikit'}`
+    console.log('Creating', chalk.green.bold(packageName))
 
-    exec(`yarn add ${packageName} --force`, (error, stdout, stderr) => {
-      if (error) {
-        console.log(`error: ${error.message}`)
-        return
-      }
-      if (stderr) {
-        console.warn(`stderr: ${stderr}`)
-        return
-      }
-      console.log(`stdout: ${stdout}`)
-      console.log(chalk.green.bold(packageName), 'successfuly added!')
-      console.log('')
-      console.log(chalk.dim.underline('Now you can import components like:'), `import { Button } from "${chalk.green.bold(packageName)}""`)
-    })
+    // exec(`git clone git@github.com:symbo-ls/starter-kit.git`, (error, stdout, stderr) => {
+    //   if (error) {
+    //     console.log(`error: ${error.message}`)
+    //     return
+    //   }
+    //   if (stderr) {
+    //     console.warn(`stderr: ${stderr}`)
+    //     return
+    //   }
+    //   console.log(`stdout: ${stdout}`)
+    //   console.log(chalk.green.bold(packageName), 'successfuly added!')
+    //   console.log('')
+    //   console.log(chalk.dim.underline('Now you can import components like:'), `import { Button } from "${chalk.green.bold(packageName)}""`)
+    // })
   })
