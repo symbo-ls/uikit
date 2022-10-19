ae2544e1f2f21fa1b5b8375246f142adfbc471d2
nikoloza <nika.tomadze@gmail.com>
2022-10-19 11:32:26 +0400

    added error

--- bin/sync.js
+++ bin/sync.js
@@ -8,7 +8,7 @@ const RC_PATH = process.cwd() + '/.symbolsrc.json'
 let rc = {}
 try {
   rc = loadModule(RC_PATH) // eslint-disable-line
-} catch (e) {}
+} catch (e) { console.error('Please include .symbolsrc.json to your root of respository') }
 
 program
   .command('sync')
