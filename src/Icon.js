df244f6abfbd894ab592ba04419f1adda6fc134d
nikoloza <nika.tomadze@gmail.com>
2022-10-21 11:54:56 +0400

    fixed icon updating

--- src/Icon.js
+++ src/Icon.js
@@ -9,20 +9,24 @@ export const Icon = {
     const { ICONS } = context && context.SYSTEM
     const iconName = props.inheritedString || props.name || props.icon || key
     const camelCase = toCamelCase(iconName)
-    const isArray = camelCase.split(/([a-z])([A-Z])/g)
 
-    const iconFromLibrary = ICONS[camelCase] || (ICONS[isArray[0] + isArray[1]]) || ICONS[isArray[0]] || ICONS['noIcon']
+    const isArray = camelCase.split(/([a-z])([A-Z])/g)
 
     let activeIconName
     if (props.active) {
       activeIconName = props['.active'].name || props['.active'].icon
     }
+    if (parent.props.active && parent.props['.active'] && parent.props['.active'].icon) {
+      activeIconName = parent.props['.active'].icon.name || parent.props['.active'].icon.icon || parent.props['.active'].icon
+    }
+
+    const iconFromLibrary = ICONS[activeIconName] || ICONS[camelCase] || (ICONS[isArray[0] + isArray[1]]) || ICONS[isArray[0]] || ICONS['noIcon']
 
     return {
       width: 'A',
       height: 'A',
       display: 'inline-block',
-      src: activeIconName || iconFromLibrary,
+      src: iconFromLibrary,
       style: { fill: 'currentColor' }
     }
   },
