50a542acf43d0b40d3b2c9720dbc23093edc8cdb
nikoloza <nika.tomadze@gmail.com>
2022-10-22 20:30:19 +0400

    fixed picture

--- src/atoms/Picture.js
+++ src/atoms/Picture.js
@@ -1,13 +1,17 @@
 'use strict'
 
+import { getSystemTheme } from './Theme' // eslint-disable-line no-unused-vars
+
 export const Picture = {
   tag: 'picture',
 
   childExtend: {
     tag: 'source',
     attr: {
-      media: ({ props, key, context }) => {
+      media: element => {
+        const { props, key, context } = element
         const { MEDIA } = context.SYSTEM
+        // getSystemTheme(element)
         return MEDIA[(props.media || key).slice(1)]
       },
       srcset: ({ props }) => props.srcset
