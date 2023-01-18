c9babce68f9509a2345e6a8da80a9abb9f50410b
nikoloza <nika.tomadze@gmail.com>
2023-01-18 11:35:21 +0400

    added scratch

--- src/utils/theme.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-export const returnSubThemeOrDefault = (orig, theme) => {
-  if (!orig) return
-  if (orig.themes && orig.themes[theme]) return orig.themes[theme]
-  if (orig[theme]) return [orig, orig[theme]]
-  return orig
-}
