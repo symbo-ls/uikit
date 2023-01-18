189bb961646d3d3b9df5f97c26ef2b0ff03ce432
nikoloza <nika.tomadze@gmail.com>
2023-01-18 11:33:11 +0400

    added cli

--- bin/program.js
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env node
-
-import { Command } from 'commander'
-export const program = new Command()
--- bin/require.js
+++
@@ -1,8 +0,0 @@
-import { createRequire } from 'module'
-
-class ImportError extends Error {} /* Bring in the ability to create the 'require' method */ // eslint-disable-line
-const require = createRequire(import.meta.url) // construct the require method
-
-export const loadModule = async (modulePath) => {
-  return require(modulePath) // use the require method
-}
